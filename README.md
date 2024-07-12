de52f2662935f135c22489e4fb5010c8c82ae57c
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2024-07-12 15:58:40 -0300

    Criando um app-agenda!

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1>Criando um app Agenda!</h1>
+<br>
+<p>Recursos novos:</p>
+<br>
+<li>ion-datetime</li>
+<li>modal</li>
+<br>
+<p>Segue Imagens do Projeto:</p>
+<br>
+<img src="../app-agenda/src/assets/home-page.png">
+<br>
+<img src="../app-agenda/src/assets/date-modal.png">
+<br>
+<img src="../app-agenda/src/assets/time-modal.png">
+<br>
+<img src="../app-agenda/src/assets/add-alert.png">
+<br>
+<img src="../app-agenda/src/assets/trash-alert.png">
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,76 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+dadosAgenda=[
+  {
+    texto:"prova de Matemática",
+    data:"2025-02-08",
+    ativo:true
+  },
 
-  constructor() {}
+  {
+    texto:"Treino de Natação",
+    data:"2025-03-22",
+    ativo:false
+  }
+]
 
+  constructor(public toastCtrl:ToastController,public alertController:AlertController) {}
+
+  ativaCompromisso (index:any) {
+    let mensagem=' Compromisso ' + this.dadosAgenda[index].texto;
+    if(this.dadosAgenda[index].ativo==true) {
+      mensagem=mensagem+' ativo';
+      this.toastCtrl.create({
+        message:mensagem,
+        duration:2000,
+      })
+      .then((toast)=>{
+        toast.present();
+      });
+    }else{
+      mensagem=mensagem+' desativado';
+      this.toastCtrl.create({
+        message:mensagem,
+        duration:2000,
+      })
+      .then((toast)=>{
+        toast.present();
+      });
+    }
+  }
+  mudar(value:boolean) {
+    this.dadosAgenda.forEach((item)=>{
+      item.ativo=value;
+    })
+  }
+  public adicionar = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
+
+  
+  async apagar() {
+    const alert = await this.alertController.create({
+      header: 'Opa!',
+      subHeader: '',
+      message: 'Realmente deseja apagar todos os compromissos?',
+      buttons: ['Sim','Não'],
+    });
+
+    await alert.present();
+  }
 }
